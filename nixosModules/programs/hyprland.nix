d26af771f04439e774585ff651a8335cb9787e55
Hamzah Mujawar <hamzah1faisal@gmail.com>
2025-08-06 00:07:25 +0100

    hyprland

--- nixosModules/programs/hyprland.nix
+++ nixosModules/programs/hyprland.nix
@@ -1,13 +1,11 @@
-{ inputs, pkgs, lib, config, ... }: {
+{ pkgs, lib, config, ... }: {
     options = {
         hyprland.enable =
             lib.mkEnableOption "enables hyprland";
     };
     config = lib.mkIf config.hyprland.enable {
-      inputs.hyprland = {
+      hyprland = {
 	enable = true;
-	package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
-	portalPackage = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
       };
     };
 }
