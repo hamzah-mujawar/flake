bfebedb03c139c38601927258f619f22f142e8bd
Hamzah Mujawar <hamzah1faisal@gmail.com>
2025-08-05 23:05:10 +0100

    Deleted gnome service completely (for real this time)

--- nixosModules/default.nix
+++ nixosModules/default.nix
@@ -1,14 +1,11 @@
 { pkgs, lib, ... }: {
   imports = [
     ./services/x11.nix
-    ./services/gnome.nix
     ./services/pipewire.nix
     ./services/dwm.nix
   ];
   
   x11.enable =
-    lib.mkDefault true;
-  gnome.enable =
     lib.mkDefault true;
   pipewire.enable =
     lib.mkDefault true;
