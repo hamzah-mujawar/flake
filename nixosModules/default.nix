334fd3de429c327471ff9df66c489beea2a5bbbe
Hamzah Mujawar <hamzah1faisal@gmail.com>
2025-08-12 16:43:31 +0100

    actually enabling it

--- nixosModules/default.nix
+++ nixosModules/default.nix
@@ -3,8 +3,10 @@
     ./services/x11.nix
     ./services/pipewire.nix
     ./services/gnome.nix
+    ./hardware/intelGraphics.nix
   ];
-  
+  intel.enable =
+    lib.mkDefault true;
   x11.enable =
     lib.mkDefault true;
   pipewire.enable =
