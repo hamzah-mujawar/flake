c431b585771bbb8bfabd71c59a725b8c5365ba6e
Hamzah Mujawar <hamzah1faisal@gmail.com>
2025-08-14 17:08:55 +0100

    squash

--- hosts/cthulhu/home.nix
+++ hosts/cthulhu/home.nix
@@ -77,7 +77,7 @@
     enable = true;
 
     settings = {
-      general = with config.colorScheme.pallete; {
+      general = with config.colorScheme.palette; {
 	"col.active_border" = "rgba(${base0E}ff) rgba(${base09}ff) 60def";
 	"col.inactive_border" = "rgba(${base00}ff)";
       };
