7a5b1f08ffc859753e909c2b4f40042112c4bf48
Hamzah Mujawar <hamzah1faisal@gmail.com>
2025-08-14 18:03:26 +0100

    one day i will squash all these superflous commits

--- hosts/cthulhu/home.nix
+++ hosts/cthulhu/home.nix
@@ -77,6 +77,7 @@
     enable = true;
 
     settings = {
+      "$terminal" = "kitty";
       general = with config.colorScheme.palette; {
 	"col.active_border" = "rgba(${base0E}ff) rgba(${base09}ff) 60deg";
 	"col.inactive_border" = "rgba(${base00}ff)";
@@ -84,12 +85,49 @@
 
       "$mod" = "SUPER";
 
+      input {
+      	"kb_layout" = "us";
+	"follow_mouse" = 1;
+	"sensitivity" = 0;
+	"touchpad" {
+		"natural_scroll" = false;
+	}
+      }
       bindm = [
 	# mouse movements
       	"$mod, mouse:272, movewindow"
 	"$mod, mouse:273, resizewindow"
 	"$mod ALT, mouse:272, resize window"
-	"$mod, Q, exec, kitty" # kitty my beloved
+	"$mod, Q, exec, $terminal" # kitty my beloved
+	"$mod, M, exit"
+	"$mod, C, killactive"
+	# focus mods
+ 	"$mod, H movefocus, l"
+	"$mod, L, movefocus, r"
+	"$mod, J, movefocus, d"
+	"$mod, K, movefocus, u"
+	# workspace mods vim G [C-A] to count up
+	"$mod, 1, workspace, 1"
+	"$mod, 2, workspace, 2"
+	"$mod, 3, workspace, 3"
+	"$mod, 4, workspace, 4"
+	"$mod, 5, workspace, 5"
+	"$mod, 6, workspace, 6"
+	"$mod, 7, workspace, 7"
+	"$mod, 8, workspace, 8"
+	"$mod, 9, workspace, 9"
+	"$mod, 0, workspace, 10"
+	# move active window to a workspace with mod + shift
+	"$mod SHIFT, 1, movetoworkspace, 1"
+	"$mod SHIFT, 2, movetoworkspace, 2"
+	"$mod SHIFT, 3, movetoworkspace, 3"
+	"$mod SHIFT, 4, movetoworkspace, 4"
+	"$mod SHIFT, 5, movetoworkspace, 5"
+	"$mod SHIFT, 6, movetoworkspace, 6"
+	"$mod SHIFT, 7, movetoworkspace, 7"
+	"$mod SHIFT, 8, movetoworkspace, 8"
+	"$mod SHIFT, 9, movetoworkspace, 9"
+	"$mod SHIFT, 0, movetoworkspace, 10"
       ];
     };
   };
