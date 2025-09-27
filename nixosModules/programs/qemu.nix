67289b6866aa4fc89da3fba0588187902d293928
Hamzah Mujawar <hamzah1faisal@gmail.com>
2025-09-27 14:24:34 +0100

    Qemu

---
+++ nixosModules/programs/qemu.nix
@@ -0,0 +1,11 @@
+{ pkgs, lib, config, ... }: {
+    options = {
+      qemu.enable =
+            lib.mkEnableOption "enables qemu";
+    };
+    config = lib.mkIf config.qemu.enable {
+      environment.systemPackages = [
+	pkgs.qemu
+      ];
+    };
+}
