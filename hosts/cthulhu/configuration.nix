20e4593721cd2f85f4a86e7a53aafb182a5d231b
Hamzah Mujawar <hamzah1faisal@gmail.com>
2026-03-04 13:43:51 +0000

    Treesitter grammars emacs

--- hosts/cthulhu/configuration.nix
+++ hosts/cthulhu/configuration.nix
@@ -48,6 +48,47 @@
 
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
+  
+  tree-sitter-parsers = grammars: with grammars; [
+    tree-sitter-bash
+    tree-sitter-c
+    tree-sitter-c-sharp
+    tree-sitter-cmake
+    tree-sitter-cpp
+    tree-sitter-css
+    tree-sitter-dot
+    tree-sitter-elisp
+    tree-sitter-glsl
+    tree-sitter-haskell
+    tree-sitter-html
+    tree-sitter-java
+    tree-sitter-javascript
+    tree-sitter-json
+    tree-sitter-json5
+    tree-sitter-kotlin
+    tree-sitter-latex
+    tree-sitter-llvm
+    tree-sitter-lua
+    tree-sitter-make
+    tree-sitter-markdown
+    tree-sitter-markdown-inline
+    tree-sitter-nickel
+    tree-sitter-nix
+    tree-sitter-prisma
+    tree-sitter-python
+    inputs.nix-qml-support.packages.${pkgs.stdenv.system}.tree-sitter-qmljs
+    tree-sitter-regex
+    tree-sitter-rust
+    tree-sitter-scss
+    tree-sitter-sql
+    tree-sitter-toml
+    tree-sitter-tsx
+    tree-sitter-typescript
+    tree-sitter-vim
+    tree-sitter-yaml
+    tree-sitter-zig
+  ];
+
 
   environment.systemPackages = with pkgs; [
     usbutils
@@ -74,7 +115,7 @@
     libtool
     typescript-language-server
     ((emacsPackagesFor emacs-pgtk).emacsWithPackages (
-      epkgs: [epkgs.vterm epkgs.treesit-grammars.with-all-grammars]
+      epkgs: [epkgs.vterm epkgs.treesit-grammars.with-grammars (grammars: tree-sitter-parsers grammars)]
     ))
     eslint_d
     man-db
