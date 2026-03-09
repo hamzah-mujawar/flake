c8ea183e870e948ea1f2234067c0df2567bd3f23
Hamzah Mujawar <hamzah1faisal@gmail.com>
2026-03-09 12:22:26 +0000

    added elephant in flake outputs

--- nixosModules/programs/walker.nix
+++ nixosModules/programs/walker.nix
@@ -10,5 +10,9 @@
       package = inputs.walker.packages.${pkgs.stdenv.hostPlatform.system}.default;      
       runAsService = true;
     };
+    programs.elephant = {
+      enable = true;
+      package = inputs.elephant.packages.${pkgs.stdenv.hostPlatform.system}.default;
+    };
   };
 }
