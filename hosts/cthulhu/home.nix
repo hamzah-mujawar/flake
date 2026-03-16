e077cbbb2b8936a71d3d2b92b5d9b17f43878529
Hamzah Mujawar <hamzah1faisal@gmail.com>
2026-03-16 07:43:50 +0000

    Miracode

--- hosts/cthulhu/home.nix
+++ hosts/cthulhu/home.nix
@@ -14,7 +14,7 @@
     # The home.packages option allows you to install Nix packages into your
     # environment.
     home.packages = with pkgs; [
-      monocraft
+      miracode
       nerd-fonts.caskaydia-mono
     ];
   
